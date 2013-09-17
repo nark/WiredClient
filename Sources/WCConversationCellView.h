720a600410c20b63bddbb6551fbd12d987521b05
Rafaël Warnault <rw@read-write.fr>
2013-09-17 23:19:04 +0200

    Remove encrypted-core-data submodule

---
+++ Sources/WCConversationCellView.h
@@ -0,0 +1,13 @@
+//
+//  WCConversationCellView.h
+//  WiredClient
+//
+//  Created by Rafaël Warnault on 16/09/13.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface WCConversationCellView : NSTableCellView
+
+@end
---
+++ Sources/WCConversationCellView.m
@@ -0,0 +1,28 @@
+//
+//  WCConversationCellView.m
+//  WiredClient
+//
+//  Created by Rafaël Warnault on 16/09/13.
+//
+//
+
+#import "WCConversationCellView.h"
+
+@implementation WCConversationCellView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
